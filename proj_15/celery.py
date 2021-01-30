b2da87d1d365dc825a04c8a33e4f57f54c12938a
andreichernyshov <andrei.terex89@gmail.com>
2021-01-31 00:09:47 +0200

    ;;;;

--- proj_15/celery.py
+++ proj_15/celery.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj_15.settings')
-celery_app = Celery('proj_15')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
+
+app = Celery('proj_15')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
